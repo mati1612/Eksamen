eee2472456b481b5304de0370fa01c91b41fbf0c
Matias Søndergaard Schjøtt <mati1612@edu.campusvejle.dk>
2017-09-12 10:57:07 +0200

    Første layout til side

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>FancyClothes.DK</title>
+    <link rel="stylesheet" href="normalize.css">
+    <link rel="stylesheet" href="skeleton.css">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="script.js"></script>
+</head>
+<body>
+<!-- Top søgebar og sprog, hardcoded -->
+    <div class="container">
+        <div class="row">
+            <div class="six columns"></div>
+            <div class="six columns"></div>
+            <div class="twelve columns"></div>
+        </div>
+    </div>
+    <hr>
+
+<!-- Navigation og indkøbskurv -->
+    <div class="container">
+        <div class="row">
+        <nav>
+            <ul class="row">
+                <li><a href="#">Foside</a></li>
+                <li><a href="#">Produkter</a></li>
+                <li><a href="#">Nyheder</a></li>
+                <li><a href="#">Handelsbetingelser</a></li>
+                <li><a href="#">Om os</a></li>
+                <li><a href="#">Log Ind</a></li>
+            </ul>
+        </nav>
+        <div id="kurv">
+        </div>
+        </div>
+    </div>
+    <hr>
+
+<!-- Slideshow -->
+    <div class="container">
+    </div>
+    <hr>
+
+<!-- Tagline/header -->
+    <header class="container">
+    </header>
+    <hr>
+
+<!-- Aside + Main container start -->
+    <div class="container">
+
+<!-- Aside bar -->
+    <aside>
+    </aside>
+
+<!-- Main artikel område -->
+    <main>
+        
+    </main>
+
+<!-- Aside + Main container slut -->
+    </div>
+    <hr>
+
+<!-- Footer -->
+    <footer class="container">
+    </footer>
+</body>
+</html>
